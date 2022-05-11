9ddd891701c289732fad71a01d459794c8daa271
sin <yuho.shin@gmail.com>
2022-05-12 06:13:45 +0900

    Initial Commit

---
+++ sin_12_maze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sin_12_maze
+//
+//  Created by 菊地英治 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
